5ff0adf3cf9f4862f4e8f6d392c71729607a0e01
Facundo Lerena <faculerena@gmail.com>
2024-01-02 11:23:01 -0300

    More detectors

--- soroban_detectors/unprotected-update-current-contract-wasm/src/lib.rs
+++ soroban_detectors/unprotected-update-current-contract-wasm/src/lib.rs
@@ -15,9 +15,8 @@ use rustc_middle::mir::{BasicBlock, BasicBlocks, Const, Operand, TerminatorKind}
 use rustc_middle::ty::TyKind;
 use rustc_span::def_id::DefId;
 use rustc_span::Span;
-use scout_audit_internal::{
-    DetectorImpl, SorobanDetector as Detector, SOROBAN_AVOID_CORE_MEM_FORGET_LINT_MESSAGE,
-};
+use scout_audit_internal::Detector;
+
 dylint_linting::impl_late_lint! {
     pub UNPROTECTED_UPDATE_CURRENT_CONTRACT_WASM,
     Warn,
