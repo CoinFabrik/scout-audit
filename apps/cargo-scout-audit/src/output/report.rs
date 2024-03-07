ed9688481b1f629d0dc171b54e1dc4bbc1f09fba
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-03-07 16:55:59 -0300

    Report fields should be public

--- apps/cargo-scout-audit/src/output/report.rs
+++ apps/cargo-scout-audit/src/output/report.rs
@@ -3,52 +3,52 @@ use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use super::html;
+use super::{html, markdown};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Report {
-    name: String,
-    description: String,
-    date: NaiveDate,
-    source_url: String,
-    summary: Summary,
-    categories: Vec<Category>,
-    findings: Vec<Finding>,
+    pub name: String,
+    pub description: String,
+    pub date: NaiveDate,
+    pub source_url: String,
+    pub summary: Summary,
+    pub categories: Vec<Category>,
+    pub findings: Vec<Finding>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Summary {
-    total_vulnerabilities: u32,
-    by_severity: HashMap<String, u32>,
+    pub total_vulnerabilities: u32,
+    pub by_severity: HashMap<String, u32>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Category {
-    id: String,
-    name: String,
-    vulnerabilities: Vec<Vulnerability>,
+    pub id: String,
+    pub name: String,
+    pub vulnerabilities: Vec<Vulnerability>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Vulnerability {
-    id: String,
-    name: String,
-    short_message: String,
-    long_message: String,
-    severity: String,
-    help: String,
+    pub id: String,
+    pub name: String,
+    pub short_message: String,
+    pub long_message: String,
+    pub severity: String,
+    pub help: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Finding {
-    id: u32,
-    occurrence_index: u32,
-    category_id: String,
-    vulnerability_id: String,
-    error_message: String,
-    span: String,
-    code_snippet: String,
-    file: String,
+    pub id: u32,
+    pub occurrence_index: u32,
+    pub category_id: String,
+    pub vulnerability_id: String,
+    pub error_message: String,
+    pub span: String,
+    pub code_snippet: String,
+    pub file: String,
 }
 
 impl Report {
@@ -74,5 +74,9 @@ impl Report {
 
     pub fn generate_html(&self) -> Result<&'static str> {
         html::generate_html(self)
+    }
+
+    pub fn generate_markdown(&self) -> Result<&'static str> {
+        markdown::generate_markdown(self)
     }
 }
