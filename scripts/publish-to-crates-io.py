3511ecf47c0421bfe48f3c95e3b0b850c17a5684
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-04-23 15:55:12 -0300

    Delete old scripts and rename clippy_config folder

--- scripts/publish-to-crates-io.py
+++ scripts/publish-to-crates-io.py
@@ -1,61 +1,49 @@
+import argparse
 from pathlib import Path
-import shutil
-import subprocess
-import tempfile
-import time
 
-def get_package_name(file_path):
-    with open(file_path, "r") as f:
-        for line in f:
-            if line.startswith("name"):
-                return line.split("=")[1].strip().strip('"')
-    raise Exception("Could not find package name in Cargo.toml")
 
-def get_package_version(file_path):
-    with open(file_path, "r") as f:
-        for line in f:
-            if line.startswith("version"):
-                return line.split("=")[1].strip().strip('"')
-    raise Exception("Could not find package version in Cargo.toml")
+def main(crate, dry_run):
+    root_path = Path(__file__).parent.parent
+    crates_paths = {
+        "scout-audit-clippy-utils": root_path / "apps" / "scout-audit-clippy-utils",
+        "scout-audit-internal": root_path / "scout-audit-internal",
+        "cargo-scout-audit": root_path / "cargo-scout-audit",
+        "scout-audit-dylint-linting": root_path / "scout-audit-dylint-linting",
+    }
 
-def is_package_published(package_name, package_version, package_path):
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        subprocess.call(["cargo", "init"], cwd=tmpdirname)
-        if (package_path / "rust-toolchain").exists():
-            shutil.copy(package_path / "rust-toolchain", Path(tmpdirname) / "rust-toolchain")
-        with open(Path(tmpdirname) / "Cargo.toml", "a") as f:
-            f.write(f'{package_name} = "={package_version}"')
-        print(f"Checking if {package_name} {package_version} is published...")
-        try:
-            subprocess.check_output(["cargo", "check"], cwd=tmpdirname)
-            return True
-        except subprocess.CalledProcessError:
-            return False
-        except:
-            raise
+    selected_crates = (
+        [crates_paths[crate]] if crate in crates_paths else crates_paths.values()
+    )
 
-def publish_package(package_path):
-    subprocess.check_output(["cargo", "publish"], cwd=package_path)
+    # for path in selected_crates:
+    #     name, version = get_crate_details(path / "Cargo.toml")
+    #     if is_crate_published(name, version, path):
+    #         print(f"{name} {version} is already published.")
+    #     else:
+    #         publish_crate(path, dry_run)
+    #         if not dry_run:
+    #             while not is_crate_published(name, version, path):
+    #                 print(f"Waiting for {name} {version} to be published...")
+    #                 time.sleep(10)
+    #             print(f"{name} {version} is published.")
+
 
 if __name__ == "__main__":
-    ROOT_PATH = Path(__file__).parent.parent
-    CARGO_SCOUT_AUDIT_PATH = ROOT_PATH / "apps" / "cargo-scout-audit"
-    SCOUT_AUDIT_CLIPPY_UTILS = ROOT_PATH / "scout-audit-clippy-utils"
-    SCOUT_AUDIT_INTERNAL_PATH = ROOT_PATH / "scout-audit-internal"
-    SCOUT_AUDIT_DYLINT_LINTING = ROOT_PATH / "scout-audit-dylint-linting"
-
-    packages_paths = [SCOUT_AUDIT_CLIPPY_UTILS, SCOUT_AUDIT_INTERNAL_PATH, CARGO_SCOUT_AUDIT_PATH, SCOUT_AUDIT_DYLINT_LINTING]
-
-    for path in packages_paths:
-        package_name = get_package_name(path / "Cargo.toml")
-        package_version = get_package_version(path / "Cargo.toml")
-        print(f"Publishing {package_name} {package_version}...")
-
-        if not is_package_published(package_name, package_version, path):
-            publish_package(path)
-            while not is_package_published(package_name, package_version, path):
-                print(f"{package_name} {package_version} is not published yet, waiting 10 seconds...")
-                time.sleep(10)
-            print(f"{package_name} {package_version} is published")
-        else:
-            print(f"{package_name} {package_version} is already published")
+    parser = argparse.ArgumentParser(description="Publish Rust crates.")
+    parser.add_argument(
+        "--crate",
+        choices=[
+            "scout-audit-clippy-utils",
+            "scout-audit-internal",
+            "cargo-scout-audit",
+            "scout-audit-dylint-linting",
+            "all",
+        ],
+        default="all",
+        help="Specify which crate to publish",
+    )
+    parser.add_argument(
+        "--dry-run", action="store_true", help="Perform a dry run without publishing."
+    )
+    args = parser.parse_args()
+    main(args.crate, args.dry_run)
