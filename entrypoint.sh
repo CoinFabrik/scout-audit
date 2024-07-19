6c1ff42983362bbdef696e62e37edfda205cf3a6
david weil <tenuki@gmail.com>
2024-07-19 11:25:43 -0300

    Update entrypoint.sh
    removed unused stuff

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,5 @@ set -e -o pipefail
 # Change to the target directory
 cd "$INPUT_TARGET"
 
-# Check if markdown output is required
-if [[ "$INPUT_MARKDOWN_OUTPUT" == "true" ]]; then
-  INPUT_SCOUT_ARGS="$INPUT_SCOUT_ARGS --output-format md --output-path /github/workspace/report.md"
-fi
-
 # Run cargo scout-audit with the provided arguments
 cargo scout-audit $INPUT_SCOUT_ARGS
