e341edd3c5a4127b65c02c8a586fdf4e163c6da2
Víctor M. González <victor.gonzalez@nektra.com>
2024-10-24 12:04:08 -0300

    Passed through clippy and cargo fmt.

--- detectors/known-vulnerabilities/src/lib.rs
+++ detectors/known-vulnerabilities/src/lib.rs
@@ -1,65 +1,69 @@
 #![feature(rustc_private)]
-extern crate rustc_session;
 extern crate rustc_lint;
+extern crate rustc_session;
 
+use cargo_metadata::MetadataCommand;
+use colored::Colorize;
+use rustsec::{
+    report::Settings, repository::git::Repository, Database, Lockfile, Report, Vulnerability,
+};
 use scout_audit_dylint_linting::LintInfo;
 use std::ffi::CString;
 use std::path::PathBuf;
-use rustsec::{
-    report::Settings, repository::git::Repository, Database, Lockfile, Report, Vulnerability
-};
-use cargo_metadata::MetadataCommand;
-use colored::Colorize;
 
-static NAME      : &str = "KNOWN_VULNERABILITIES";
-static DESC      : &str = "This dependency has known vulnerabilities. Consider updating it or removing it.";
+static NAME: &str = "KNOWN_VULNERABILITIES";
+static DESC: &str =
+    "This dependency has known vulnerabilities. Consider updating it or removing it.";
 static LONG      : &str = "This dependency has known vulnerabilities. Consider updating it or removing it. A link to the full description of the vulnerability is provided.";
-static SEVERITY  : &str = "Medium";
-static HELP      : &str = "https://coinfabrik.github.io/scout/docs/vulnerabilities/known-vulnerabilities";
+static SEVERITY: &str = "Medium";
+static HELP: &str = "https://coinfabrik.github.io/scout/docs/vulnerabilities/known-vulnerabilities";
 static VULN_CLASS: &str = "TBD";
 
-fn string_to_c(s: &str) -> CString{
+fn string_to_c(s: &str) -> CString {
     CString::new(s.as_bytes()).unwrap()
 }
 
-fn construct_metadata() -> LintInfo{
-    LintInfo{
-        id:                  string_to_c(NAME.to_lowercase().as_str()),
-        name:                string_to_c(NAME),
-        short_message:       string_to_c(DESC),
-        long_message:        string_to_c(LONG),
-        severity:            string_to_c(SEVERITY),
-        help:                string_to_c(HELP),
+fn construct_metadata() -> LintInfo {
+    LintInfo {
+        id: string_to_c(NAME.to_lowercase().as_str()),
+        name: string_to_c(NAME),
+        short_message: string_to_c(DESC),
+        long_message: string_to_c(LONG),
+        severity: string_to_c(SEVERITY),
+        help: string_to_c(HELP),
         vulnerability_class: string_to_c(VULN_CLASS),
     }
 }
 
 #[no_mangle]
-pub fn lint_info(info: &mut LintInfo){
+pub fn lint_info(info: &mut LintInfo) {
     *info = construct_metadata();
 }
 
 #[doc(hidden)]
 #[no_mangle]
 pub extern "C" fn dylint_version() -> *mut std::os::raw::c_char {
-    std::ffi::CString::new("0.1.0")
-        .unwrap()
-        .into_raw()
+    std::ffi::CString::new("0.1.0").unwrap().into_raw()
 }
 
 #[no_mangle]
-pub fn register_lints(_: &rustc_session::Session, _: &mut rustc_lint::LintStore){}
+pub fn register_lints(_: &rustc_session::Session, _: &mut rustc_lint::LintStore) {}
 
 #[no_mangle]
-pub fn custom_detector(){
+pub fn custom_detector() {
     let _ = perform_checking();
 }
 
-fn report_vuln(vuln: &Vulnerability, toml: &PathBuf, krate: &Option<String>) -> Result<(), ()>{
+fn report_vuln(vuln: &Vulnerability, toml: &PathBuf, krate: &Option<String>) -> Result<(), ()> {
     let port = std::env::var("SCOUT_PORT_NUMBER").map_err(|_| ())?;
 
-    let short_message = format!("Known vulnerability in {} version {}", vuln.package.name, vuln.package.version);
-    let first_line = format!("{}: {}", "warning".yellow(), short_message).bold().to_string();
+    let short_message = format!(
+        "Known vulnerability in {} version {}",
+        vuln.package.name, vuln.package.version
+    );
+    let first_line = format!("{}: {}", "warning".yellow(), short_message)
+        .bold()
+        .to_string();
     let rendered = format!(
         concat!(
             "{}\n",
@@ -75,36 +79,40 @@ fn report_vuln(vuln: &Vulnerability, toml: &PathBuf, krate: &Option<String>) -> 
         "Description".bold(),
         vuln.advisory.title,
         "URL".bold(),
-        vuln.advisory.url.clone().and_then(|x| Some(x.to_string())).unwrap_or("N/A".to_string()),
+        vuln.advisory
+            .url
+            .clone()
+            .map(|x| x.to_string())
+            .unwrap_or("N/A".to_string()),
     );
     let body = serde_json::json!({
-            "crate": krate,
-            "message": {
-                "code": {
-                    "code": NAME.to_lowercase(),
-                    "explanation": null,
-                },
-                "message": short_message,
-                "rendered": rendered,
-                "spans": [
-                    {
-                        "byte_start": 0,
-                        "byte_end": 0,
-                        "line_start": 0,
-                        "line_end": 0,
-                        "column_start": 0,
-                        "column_end": 0,
-                        "expansion": null,
-                        "file_name": toml,
-                        "is_primary": true,
-                        "suggested_replacement": null,
-                        "suggestion_applicability": null,
-                        "text": null,
-                    }
-                ],
+        "crate": krate,
+        "message": {
+            "code": {
+                "code": NAME.to_lowercase(),
+                "explanation": null,
             },
-        })
-        .to_string();
+            "message": short_message,
+            "rendered": rendered,
+            "spans": [
+                {
+                    "byte_start": 0,
+                    "byte_end": 0,
+                    "line_start": 0,
+                    "line_end": 0,
+                    "column_start": 0,
+                    "column_end": 0,
+                    "expansion": null,
+                    "file_name": toml,
+                    "is_primary": true,
+                    "suggested_replacement": null,
+                    "suggestion_applicability": null,
+                    "text": null,
+                }
+            ],
+        },
+    })
+    .to_string();
 
     let _ = reqwest::blocking::Client::new()
         .post(format!("http://127.0.0.1:{port}/vuln"))
@@ -114,19 +122,23 @@ fn report_vuln(vuln: &Vulnerability, toml: &PathBuf, krate: &Option<String>) -> 
     Ok(())
 }
 
-fn perform_checking_on_dir(lock: &str, toml: &PathBuf, krate: Option<String>) -> Result<(), rustsec::Error>{
+fn perform_checking_on_dir(
+    lock: &str,
+    toml: &PathBuf,
+    krate: Option<String>,
+) -> Result<(), rustsec::Error> {
     let repo = Repository::fetch_default_repo()?;
     let database = Database::load_from_repo(&repo)?;
     let lockfile = Lockfile::load(lock)?;
     let settings = Settings::default();
     let report = Report::generate(&database, &lockfile, &settings);
-    for vuln in report.vulnerabilities.list.iter(){
+    for vuln in report.vulnerabilities.list.iter() {
         let _ = report_vuln(vuln, toml, &krate);
     }
     Ok(())
 }
 
-fn package_name_to_crate_name(s: &String) -> String {
+fn package_name_to_crate_name(s: &str) -> String {
     let mut ret = String::new();
     ret.reserve(s.len());
     for c in s.chars() {
@@ -135,15 +147,18 @@ fn package_name_to_crate_name(s: &String) -> String {
     ret
 }
 
-fn get_lock_path_and_crate(toml: &PathBuf) -> Result<(PathBuf, Option<String>), ()>{
+fn get_lock_path_and_crate(toml: &PathBuf) -> Result<(PathBuf, Option<String>), ()> {
     let mut metadata_command = MetadataCommand::new();
     metadata_command.manifest_path(toml);
     let metadata = metadata_command.exec().map_err(|_| ())?;
-    let krate = metadata.workspace_default_packages().first().and_then(|x| Some(package_name_to_crate_name(&x.name)));
+    let krate = metadata
+        .workspace_default_packages()
+        .first()
+        .map(|x| package_name_to_crate_name(&x.name));
     Ok((metadata.workspace_root.into(), krate))
 }
 
-fn perform_checking() -> Result<(), ()>{
+fn perform_checking() -> Result<(), ()> {
     let mut toml = std::env::current_dir().map_err(|_| ())?;
     toml.push("Cargo.toml");
     let (mut lock, krate) = get_lock_path_and_crate(&toml)?;
--- test-cases/integer-overflow-or-underflow/vulnerable/vulnerable-2/src/tests.rs
+++ test-cases/integer-overflow-or-underflow/vulnerable/vulnerable-2/src/tests.rs
@@ -19,16 +19,16 @@
 
 use crate::*;
 use frame_support::{
-	assert_ok, derive_impl,
-	dispatch::{DispatchInfo, GetDispatchInfo},
-	traits::{ConstU64, OnInitialize},
+    assert_ok, derive_impl,
+    dispatch::{DispatchInfo, GetDispatchInfo},
+    traits::{ConstU64, OnInitialize},
 };
 use sp_core::H256;
 // The testing primitives are very useful for avoiding having to work with signatures
 // or public keys. `u64` is used as the `AccountId` and no `Signature`s are required.
 use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup},
-	BuildStorage,
+    traits::{BlakeTwo256, IdentityLookup},
+    BuildStorage,
 };
 // Reexport crate as its pallet name for construct_runtime.
 use crate as pallet_example_basic;
@@ -37,150 +37,150 @@ type Block = frame_system::mocking::MockBlock<Test>;
 
 // For testing the pallet, we construct a mock runtime.
 frame_support::construct_runtime!(
-	pub enum Test
-	{
-		System: frame_system,
-		Balances: pallet_balances,
-		Example: pallet_example_basic,
-	}
+    pub enum Test
+    {
+        System: frame_system,
+        Balances: pallet_balances,
+        Example: pallet_example_basic,
+    }
 );
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
 impl frame_system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type Nonce = u64;
-	type Hash = H256;
-	type RuntimeCall = RuntimeCall;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Block = Block;
-	type RuntimeEvent = RuntimeEvent;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = pallet_balances::AccountData<u64>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ();
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type Nonce = u64;
+    type Hash = H256;
+    type RuntimeCall = RuntimeCall;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = Block;
+    type RuntimeEvent = RuntimeEvent;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<u64>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
 #[derive_impl(pallet_balances::config_preludes::TestDefaultConfig)]
 impl pallet_balances::Config for Test {
-	type AccountStore = System;
+    type AccountStore = System;
 }
 
 impl Config for Test {
-	type MagicNumber = ConstU64<1_000_000_000>;
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = ();
+    type MagicNumber = ConstU64<1_000_000_000>;
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
 }
 
 // This function basically just builds a genesis storage key/value store according to
 // our desired mockup.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	let t = RuntimeGenesisConfig {
-		// We use default for brevity, but you can configure as desired if needed.
-		system: Default::default(),
-		balances: Default::default(),
-		example: pallet_example_basic::GenesisConfig {
-			dummy: 42,
-			// we configure the map with (key, value) pairs.
-			bar: alloc::vec![(1, 2), (2, 3)],
-			foo: 24,
-		},
-	}
-	.build_storage()
-	.unwrap();
-	t.into()
+    let t = RuntimeGenesisConfig {
+        // We use default for brevity, but you can configure as desired if needed.
+        system: Default::default(),
+        balances: Default::default(),
+        example: pallet_example_basic::GenesisConfig {
+            dummy: 42,
+            // we configure the map with (key, value) pairs.
+            bar: alloc::vec![(1, 2), (2, 3)],
+            foo: 24,
+        },
+    }
+    .build_storage()
+    .unwrap();
+    t.into()
 }
 
 #[test]
 fn it_works_for_optional_value() {
-	new_test_ext().execute_with(|| {
-		// Check that GenesisBuilder works properly.
-		let val1 = 42;
-		let val2 = 27;
-		assert_eq!(Dummy::<Test>::get(), Some(val1));
+    new_test_ext().execute_with(|| {
+        // Check that GenesisBuilder works properly.
+        let val1 = 42;
+        let val2 = 27;
+        assert_eq!(Dummy::<Test>::get(), Some(val1));
 
-		// Check that accumulate works when we have Some value in Dummy already.
-		assert_ok!(Example::multiply_dummy(RuntimeOrigin::signed(1), val2));
-		assert_eq!(Dummy::<Test>::get(), Some(val1 * val2));
+        // Check that accumulate works when we have Some value in Dummy already.
+        assert_ok!(Example::multiply_dummy(RuntimeOrigin::signed(1), val2));
+        assert_eq!(Dummy::<Test>::get(), Some(val1 * val2));
 
-		// Check that accumulate works when we Dummy has None in it.
-		<Example as OnInitialize<u64>>::on_initialize(2);
-		assert_ok!(Example::multiply_dummy(RuntimeOrigin::signed(1), val1));
-		assert_eq!(Dummy::<Test>::get(), Some(val1 * val2 * val1));
-	});
+        // Check that accumulate works when we Dummy has None in it.
+        <Example as OnInitialize<u64>>::on_initialize(2);
+        assert_ok!(Example::multiply_dummy(RuntimeOrigin::signed(1), val1));
+        assert_eq!(Dummy::<Test>::get(), Some(val1 * val2 * val1));
+    });
 }
 
 #[test]
 fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		assert_eq!(Foo::<Test>::get(), 24);
-		assert_ok!(Example::accumulate_foo(RuntimeOrigin::signed(1), 1));
-		assert_eq!(Foo::<Test>::get(), 25);
-	});
+    new_test_ext().execute_with(|| {
+        assert_eq!(Foo::<Test>::get(), 24);
+        assert_ok!(Example::accumulate_foo(RuntimeOrigin::signed(1), 1));
+        assert_eq!(Foo::<Test>::get(), 25);
+    });
 }
 
 #[test]
 fn set_dummy_works() {
-	new_test_ext().execute_with(|| {
-		let test_val = 133;
-		assert_ok!(Example::set_dummy(RuntimeOrigin::root(), test_val.into()));
-		assert_eq!(Dummy::<Test>::get(), Some(test_val));
-	});
+    new_test_ext().execute_with(|| {
+        let test_val = 133;
+        assert_ok!(Example::set_dummy(RuntimeOrigin::root(), test_val));
+        assert_eq!(Dummy::<Test>::get(), Some(test_val));
+    });
 }
 
 #[test]
 fn signed_ext_watch_dummy_works() {
-	new_test_ext().execute_with(|| {
-		let call = pallet_example_basic::Call::set_dummy { new_value: 10 }.into();
-		let info = DispatchInfo::default();
+    new_test_ext().execute_with(|| {
+        let call = pallet_example_basic::Call::set_dummy { new_value: 10 }.into();
+        let info = DispatchInfo::default();
 
-		assert_eq!(
-			WatchDummy::<Test>(PhantomData)
-				.validate(&1, &call, &info, 150)
-				.unwrap()
-				.priority,
-			u64::MAX,
-		);
-		assert_eq!(
-			WatchDummy::<Test>(PhantomData).validate(&1, &call, &info, 250),
-			InvalidTransaction::ExhaustsResources.into(),
-		);
-	})
+        assert_eq!(
+            WatchDummy::<Test>(PhantomData)
+                .validate(&1, &call, &info, 150)
+                .unwrap()
+                .priority,
+            u64::MAX,
+        );
+        assert_eq!(
+            WatchDummy::<Test>(PhantomData).validate(&1, &call, &info, 250),
+            InvalidTransaction::ExhaustsResources.into(),
+        );
+    })
 }
 
 #[test]
 fn counted_map_works() {
-	new_test_ext().execute_with(|| {
-		assert_eq!(CountedMap::<Test>::count(), 0);
-		CountedMap::<Test>::insert(3, 3);
-		assert_eq!(CountedMap::<Test>::count(), 1);
-	})
+    new_test_ext().execute_with(|| {
+        assert_eq!(CountedMap::<Test>::count(), 0);
+        CountedMap::<Test>::insert(3, 3);
+        assert_eq!(CountedMap::<Test>::count(), 1);
+    })
 }
 
 #[test]
 fn weights_work() {
-	// must have a defined weight.
-	let default_call = pallet_example_basic::Call::<Test>::multiply_dummy { factor: 10 };
-	let info1 = default_call.get_dispatch_info();
-	// aka. `let info = <Call<Test> as GetDispatchInfo>::get_dispatch_info(&default_call);`
-	// TODO: account for proof size weight
-	assert!(info1.weight.ref_time() > 0);
-	assert_eq!(info1.weight, <Test as Config>::WeightInfo::multiply_dummy());
+    // must have a defined weight.
+    let default_call = pallet_example_basic::Call::<Test>::multiply_dummy { factor: 10 };
+    let info1 = default_call.get_dispatch_info();
+    // aka. `let info = <Call<Test> as GetDispatchInfo>::get_dispatch_info(&default_call);`
+    // TODO: account for proof size weight
+    assert!(info1.weight.ref_time() > 0);
+    assert_eq!(info1.weight, <Test as Config>::WeightInfo::multiply_dummy());
 
-	// `set_dummy` is simpler than `accumulate_dummy`, and the weight
-	//   should be less.
-	let custom_call = pallet_example_basic::Call::<Test>::set_dummy { new_value: 20 };
-	let info2 = custom_call.get_dispatch_info();
-	// TODO: account for proof size weight
-	assert!(info1.weight.ref_time() > info2.weight.ref_time());
+    // `set_dummy` is simpler than `accumulate_dummy`, and the weight
+    //   should be less.
+    let custom_call = pallet_example_basic::Call::<Test>::set_dummy { new_value: 20 };
+    let info2 = custom_call.get_dispatch_info();
+    // TODO: account for proof size weight
+    assert!(info1.weight.ref_time() > info2.weight.ref_time());
 }
