bd3279b05c25bda530547f36bb612b10592ef962
Víctor M. González <victor.gonzalez@nektra.com>
2024-11-01 10:13:35 -0300

    Added corrected scripts.

--- scripts/run-tests.py
+++ scripts/run-tests.py
@@ -1,74 +1,80 @@
-import json
 import os
-import re
-import subprocess
 import argparse
 import time
+import tempfile
+import json
+
+from utils import (
+    parse_json_from_string,
+    print_errors,
+    print_results,
+    run_subprocess,
+    is_rust_project,
+)
 
 RED = "\033[91m"
 GREEN = "\033[92m"
-BLUE = "\033[94m"
 ENDC = "\033[0m"
 
+def run_tests(detector):
+    errors = []
+    directory = os.path.join("test-cases", detector)
+    print(f"\n{GREEN}Performing tests in {directory}:{ENDC}")
+    if not os.path.exists(directory):
+        print(f"{RED}The specified directory does not exist.{ENDC}")
+        return errors
 
-def parse_json_from_string(console_output):
-    brace_count = 0
-    json_start = None
-    json_end = None
+    for root, _, _ in os.walk(directory):
+        if is_rust_project(root):
+            if run_unit_tests(root):
+                errors.append(root)
+            if not run_integration_tests(detector, root):
+                errors.append(root)
+    return errors
 
-    for i, char in enumerate(console_output):
-        if char == "{":
-            brace_count += 1
-            if brace_count == 1:
-                json_start = i
-        elif char == "}":
-            brace_count -= 1
-            if brace_count == 0 and json_start is not None:
-                json_end = i + 1
-                break
-
-    if json_start is not None and json_end is not None:
-        json_str = console_output[json_start:json_end]
-        try:
-            return json.loads(json_str)
-        except json.JSONDecodeError:
-            return "Extracted string is not valid JSON"
-    else:
-        return "No JSON found in the console output"
-
+def convert_code(s):
+    return s.replace('_', '-')
 
 def run_unit_tests(root):
     start_time = time.time()
-    result = subprocess.run(
-        ["cargo", "test", "+stable", "--all"],
-        cwd=root,
-        capture_output=True,
-        text=True,
+    returncode, stdout, _ = run_subprocess(["cargo", "test", "--all-features"], root)
+    print_results(
+        returncode,
+        stdout,
+        "unit-test",
+        root,
+        time.time() - start_time,
     )
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"{BLUE}[> {elapsed_time:.2f} sec]{ENDC} - Completed unit test in: {root}.")
-    if result.returncode != 0:
-        print(f"\n{RED}Test error found in: {root}{ENDC}\n")
-        error_message = result.stdout.strip()
-        for line in error_message.split("\n"):
-            print(f"| {line}")
-        print("\n")
-        return True
-    return False
+    return returncode != 0
+
 
 
 def run_integration_tests(detector, root):
     start_time = time.time()
-    short_message = ""
-    detector_metadata_result = subprocess.run(
-        ["cargo", "scout-audit", "--filter", detector, "--metadata"],
-        cwd=root,
-        capture_output=True,
-        text=True,
+
+    local_detectors = os.path.join(os.getcwd(), "detectors")
+
+    returncode, stdout, _ = run_subprocess(
+        [
+            "cargo",
+            "scout-audit",
+            "--filter",
+            detector,
+            "--metadata",
+            "--local-detectors",
+            local_detectors,
+        ],
+        root,
     )
 
-    detector_metadata = parse_json_from_string(detector_metadata_result.stdout)
+    if stdout is None:
+        print(
+            f"{RED}Failed to run integration tests in {root} - Metadata returned empty.{ENDC}"
+        )
+        return True
+
+    detector_metadata = parse_json_from_string(stdout)
+
     if not isinstance(detector_metadata, dict):
         print("Failed to extract JSON:", detector_metadata)
         return True
@@ -76,55 +82,67 @@ def run_integration_tests(detector, root):
     detector_key = detector.replace("-", "_")
     short_message = detector_metadata.get(detector_key, {}).get("short_message")
 
-    result = subprocess.run(
-        ["cargo", "scout-audit", "--filter", detector],
-        cwd=root,
-        capture_output=True,
-        text=True,
-    )
+    _, tempPath = tempfile.mkstemp(None, f'scout_{os.getpid()}_')
 
-    elapsed_time = time.time() - start_time
-    print(
-        f"{BLUE}[> {elapsed_time:.2f} sec]{ENDC} - Completed integration test in: {root}."
-    )
+    returncode = None
+    stderr = None
 
-    should_lint = root.endswith("vulnerable-example")
-    if result.returncode != 0 or (
-        should_lint and short_message and short_message not in result.stderr
-    ):
-        print(f"\n{RED}Test error found in: {root}{ENDC}\n")
-        error_message = result.stderr.strip()
-        for line in error_message.split("\n"):
-            print(f"| {line}")
-        print("\n")
-        return True
-
-    return False
-
-
-def run_tests(detector):
-    errors = []
-    directory = os.path.join("test-cases", detector)
-    print(f"\n{GREEN}Performing tests in {directory}:{ENDC}")
-    if os.path.exists(directory):
-        for root, _, files in os.walk(directory):
-            if "Cargo.toml" in files:
-                if run_unit_tests(root):
-                    errors.append(root)
-                if run_integration_tests(detector, root):
-                    errors.append(root)
+    if detector != "unnecessary-lint-allow":
+        returncode, _, stderr = run_subprocess(
+            [
+                "cargo",
+                "scout-audit",
+                "--filter",
+                detector,
+                "--local-detectors",
+                local_detectors,
+                "--output-format",
+                "raw-json",
+                "--output-path",
+                tempPath,
+            ],
+            root,
+        )
     else:
-        print(f"{RED}The specified directory does not exist.{ENDC}")
-    return errors
+        #We need to handle this case differently, because using filter will
+        #cause other detectors to not run, making the test case invalid.
+        returncode, _, stderr = run_subprocess(
+            [
+                "cargo",
+                "scout-audit",
+                "--local-detectors",
+                local_detectors,
+                "--output-format",
+                "raw-json",
+                "--output-path",
+                tempPath,
+            ],
+            root,
+        )
 
+    if returncode != 0:
+        print(f"{RED}Scout failed to run.{ENDC}")
+        return False
+    
+    should_fail = "vulnerable" in root
+    did_fail = False
 
-def print_tests_errors(errors):
-    if errors:
-        print(f"{RED}\nErrors detected in the following directories:{ENDC}")
-        for error_dir in errors:
-            print(f"• {error_dir}")
-    else:
-        print(f"{GREEN}\nNo errors found in the specified directory.{ENDC}")
+    with open(tempPath) as file:
+        detectors_triggered = {convert_code(json.loads(line.rstrip())['code']['code']) for line in file}
+        did_fail = detector in detectors_triggered
+        if should_fail != did_fail:
+            explanation = "it failed when it shouldn't have" if did_fail else "it didn't fail when it should have"
+            print(f"{RED}Test case {root} didn't pass because {explanation}.{ENDC}")
+            return False
+
+    print_results(
+        returncode,
+        stderr,
+        "integration-test",
+        root,
+        time.time() - start_time,
+    )
+    return True
 
 
 if __name__ == "__main__":
@@ -132,15 +150,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--detector",
         type=str,
+        required=True,
         help='The detector to run tests for, e.g., "unsafe-unwrap"',
     )
-
     args = parser.parse_args()
 
-    if args.detector:
-        errors = run_tests(args.detector)
-        print_tests_errors(errors)
-        if errors:
-            exit(1)
-    else:
-        print(f"{RED}No detector specified. Please provide a detector argument.{ENDC}")
+    errors = run_tests(args.detector)
+    print_errors(errors)
+    if errors:
+        exit(1)
