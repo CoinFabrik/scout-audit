eddfc005b1da8c4ab290281fffd9ab839a0ad18f
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-11-06 18:03:12 -0300

    Use clippy_wrappers for span_lint

--- detectors/token-interface-inference/src/lib.rs
+++ detectors/token-interface-inference/src/lib.rs
@@ -4,7 +4,7 @@ extern crate rustc_errors;
 extern crate rustc_hir;
 extern crate rustc_span;
 
-use clippy_utils::diagnostics::span_lint;
+use clippy_wrappers::span_lint;
 use edit_distance::edit_distance;
 use if_chain::if_chain;
 use rustc_errors::MultiSpan;
