e55e11ff3083a3b9dd69d3a5fb16208f7d9a06f7
José García Crosta <jgcrosta@gmail.com>
2024-11-14 15:44:32 -0300

    Update find-test-cases.py

--- scripts/find-test-cases.py
+++ scripts/find-test-cases.py
@@ -2,6 +2,42 @@ import utils
 import json
 import sys
 import argparse
+import re
+
+def list_files_touched(base_ref, head_ref):
+    code, out, err = utils.run_subprocess(['git', 'diff', '--raw', f'{base_ref}..{head_ref}'], ".")
+    if code != 0:
+        print('git diff failed: ' + err)
+        exit(code)
+
+    ret = []
+    out = out if out != None else ''
+    for line in out.split('\n'):
+        if len(line) == 0:
+            continue
+        match = re.match(r'^\:\d{6} \d{6} [0-9a-fA-F]{8} [0-9a-fA-F]{8} [A-Z]\t(.+)$', line)
+        if not match:
+            print(f'Failed to parse diff line {line}')
+            exit(-1)
+        ret.append(match.group(1))
+
+    return ret
+
+def any_file_touched(files_touched, blockchain, detector):
+    p1 = f'^test\\-cases\\/{re.escape(blockchain)}\\/{re.escape(detector)}\\/.*'
+    p2 = f'^detectors\\/{re.escape(blockchain)}\\/{re.escape(detector)}\\/.*'
+    for ft in files_touched:
+        if re.match(p1, ft) or re.match(p2, ft):
+            return True
+    return False
+
+def filter_test_cases(test_cases, files_touched):
+    ret = []
+    for test_case in test_cases:
+        [blockchain, detector] = test_case.split('/')
+        if any_file_touched(files_touched, blockchain, detector):
+            ret.append(test_case)
+    return ret
 
 
 def print_json(test_cases):
