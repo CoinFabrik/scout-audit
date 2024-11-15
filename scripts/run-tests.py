d7443440fc42c20e1fc9e562c7e3a05e0e279964
Jose Garcia Crosta <jgcrosta@gmail.com>
2024-11-15 10:57:33 -0300

    Rollback changes

--- scripts/run-tests.py
+++ scripts/run-tests.py
@@ -15,19 +15,9 @@ if __name__ == "__main__":
         required=True,
         help='The detector to run tests for, e.g., "unsafe-unwrap"',
     )
-    parser.add_argument(
-        "--detectors",
-        type=str,
-        required=True,
-        help="The detectors to run tests for, e.g., 'unsafe-unwrap,unsafe-unwrap-2'",
-    )
     args = parser.parse_args()
 
-    if args.detectors:
-        detectors = args.detectors.split(",")
-    else:
-        detectors = [args.detector]
-    errors = run_tests(detectors)
+    errors = run_tests(args.detector)
     print_errors(errors)
     if errors:
         exit(1)
