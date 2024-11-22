a1e5e36f9a9e962be9a0d031c7a6559e0d2b6edf
Víctor M. González <victor.gonzalez@nektra.com>
2024-11-22 20:22:05 -0300

    Modified test runner.

--- scripts/cs/run-tests/Program.cs
+++ scripts/cs/run-tests/Program.cs
@@ -4,9 +4,23 @@
     {
         static int Main(string[] args)
         {
-            var pair = new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, Environment.ProcessorCount);
-            var cases = TestUtils.ListTestCases().ToList();
-            var errors = TestUtils.RunManyTests(cases, pair.ConcurrentScheduler)?.ToList();
+            List<string>? errors = null;
+            if (args.Length == 0)
+            {
+                var pair = new ConcurrentExclusiveSchedulerPair(TaskScheduler.Default, Environment.ProcessorCount);
+                var cases = TestUtils.ListTestCases().ToList();
+                errors = TestUtils.RunManyTests(cases, pair.ConcurrentScheduler)?.ToList();
+                
+            }
+            else
+            {
+                errors = new List<string>();
+                foreach (var detector in args)
+                    errors.AddRange(TestUtils.RunTests(detector));
+                if (errors.Count == 0)
+                    errors = null;
+            }
+
             if (errors == null)
                 return -1;
             TestUtils.PrintErrors(errors);
