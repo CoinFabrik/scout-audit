522eae793e9328e44d2d98c847deea3ced01d344
fpereira24 <francisco.pereira@coinfabrik.com>
2025-01-23 11:05:37 -0300

    fix rust format

--- test-cases/substrate-pallets/equal-addresses/remediated/remediated-2/src/lib.rs
+++ test-cases/substrate-pallets/equal-addresses/remediated/remediated-2/src/lib.rs
@@ -10,7 +10,7 @@ pub use weights::*;
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
-    use frame_support::{ pallet_prelude::*, traits::BuildGenesisConfig };
+    use frame_support::{pallet_prelude::*, traits::BuildGenesisConfig};
     use frame_system::pallet_prelude::*;
 
     #[pallet::config]
