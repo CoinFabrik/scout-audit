1ee1f1883a5f1ba0604475697168e9277a1085d3
Víctor M. González <helios.vmg@gmail.com>
2025-07-04 14:47:34 -0300

    Copied files.

---
+++ nightly/2025-06-26/detectors/ink/set-contract-storage/src/lib.rs
@@ -0,0 +1,133 @@
+#![feature(rustc_private)]
+
+extern crate rustc_hir;
+extern crate rustc_span;
+
+use common::{
+    declarations::{Severity, VulnerabilityClass},
+    macros::expose_lint_info,
+};
+use if_chain::if_chain;
+use rustc_hir::{
+    def_id::LocalDefId,
+    intravisit::{walk_expr, FnKind, Visitor},
+    Body, Expr, ExprKind, FnDecl, QPath,
+};
+use rustc_lint::{LateContext, LateLintPass};
+use rustc_span::Span;
+
+const LINT_MESSAGE:&str = "Abitrary users should not have control over keys because it implies writing any value of left mapping, lazy variable, or the main struct of the contract located in position 0 of the storage";
+
+#[expose_lint_info]
+pub static SET_CONTRACT_STORAGE_INFO: LintInfo = LintInfo {
+    name: env!("CARGO_PKG_NAME"),
+    short_message: LINT_MESSAGE,
+    long_message: "In ink! the function set_contract_storage(key: &K, value: &V) can be used to modify the contract storage under a given key. When a smart contract uses this function, the contract needs to check if the caller should be able to alter this storage. If this does not happen, an arbitary caller may modify balances and other relevant contract storage.    ",
+    severity: Severity::Critical,
+    help: "https://coinfabrik.github.io/scout-audit/docs/detectors/ink/set-contract-storage",
+    vulnerability_class: VulnerabilityClass::Authorization,
+};
+
+dylint_linting::declare_late_lint! {
+    pub SET_CONTRACT_STORAGE,
+    Warn,
+    LINT_MESSAGE
+}
+
+fn expr_check_owner(cx: &LateContext, expr: &Expr) -> bool {
+    let expr_type = common::analysis::get_node_type_opt(cx, &expr.hir_id);
+    if let Some(expr_type) = expr_type {
+        expr_type
+            .to_string()
+            .contains("ink::ink_primitives::AccountId")
+    } else {
+        false
+    }
+}
+
+fn expr_check_caller(expr: &Expr) -> bool {
+    if let ExprKind::MethodCall(func, ..) = expr.kind {
+        let function_name = func.ident.name.to_string();
+        function_name.contains("caller")
+    } else {
+        false
+    }
+}
+
+struct SetContractStorageVisitor<'tcx, 'a> {
+    span: Option<Span>,
+    cx: &'a LateContext<'tcx>,
+    unprotected: bool,
+    in_conditional: bool,
+    has_caller_in_if: bool,
+    has_owner_in_if: bool,
+    has_set_contract: bool,
+}
+
+impl<'tcx, 'a> Visitor<'tcx> for SetContractStorageVisitor<'tcx, 'a> {
+    fn visit_expr(&mut self, expr: &'tcx Expr<'_>) {
+        if self.in_conditional {
+            if let ExprKind::Binary(_, left, right) = &expr.kind {
+                self.has_owner_in_if =
+                    expr_check_owner(self.cx, right) || expr_check_owner(self.cx, left);
+                self.has_caller_in_if = expr_check_caller(right) || expr_check_caller(left);
+            }
+        }
+        if let ExprKind::If(..) = &expr.kind {
+            self.in_conditional = true;
+            walk_expr(self, expr);
+            self.in_conditional = false;
+        } else if let ExprKind::Call(callee, _) = expr.kind {
+            if_chain! {
+                if let ExprKind::Path(method_path) = &callee.kind;
+                if let QPath::Resolved(None, path) = *method_path;
+                if path.segments.len() == 2;
+                if path.segments[0].ident.name.as_str() == "env";
+                if path.segments[1].ident.name.as_str() == "set_contract_storage";
+                then {
+                    self.has_set_contract = true;
+                    if !self.in_conditional && (!self.has_owner_in_if || !self.has_caller_in_if) {
+                            self.unprotected = true;
+                            self.span = Some(expr.span);
+                    }
+                }
+            }
+        }
+        walk_expr(self, expr);
+    }
+}
+
+impl<'tcx> LateLintPass<'tcx> for SetContractStorage {
+    fn check_fn(
+        &mut self,
+        cx: &LateContext<'tcx>,
+        _: FnKind<'tcx>,
+        _: &'tcx FnDecl<'_>,
+        body: &'tcx Body<'_>,
+        _: Span,
+        _: LocalDefId,
+    ) {
+        let mut reentrant_storage = SetContractStorageVisitor {
+            span: None,
+            cx,
+            unprotected: false,
+            in_conditional: false,
+            has_caller_in_if: false,
+            has_owner_in_if: false,
+            has_set_contract: false,
+        };
+
+        walk_expr(&mut reentrant_storage, body.value);
+
+        if reentrant_storage.has_set_contract && reentrant_storage.unprotected {
+            clippy_utils::diagnostics::span_lint_and_help(
+                cx,
+                SET_CONTRACT_STORAGE,
+                reentrant_storage.span.unwrap(),
+                LINT_MESSAGE,
+                None,
+                "Set access control and proper authorization validation for the set_contract_storage() function"
+            );
+        }
+    }
+}
---
+++ nightly/2025-06-26/detectors/rust/overflow-check/src/lib.rs
@@ -0,0 +1,135 @@
+#![feature(rustc_private)]
+
+extern crate rustc_ast;
+extern crate rustc_span;
+
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+    process::Command,
+    str::from_utf8,
+};
+
+use clippy_utils::diagnostics::span_lint_and_help;
+use common::{
+    declarations::{Severity, VulnerabilityClass},
+    macros::expose_lint_info,
+};
+use rustc_ast::Crate;
+use rustc_lint::{EarlyContext, EarlyLintPass, LintContext};
+use rustc_span::DUMMY_SP;
+use toml::Value;
+
+const LINT_MESSAGE: &str = "Use `overflow-checks = true` in Cargo.toml profile";
+
+#[expose_lint_info]
+pub static OVERFLOW_CHECK_INFO: LintInfo = LintInfo {
+    name: env!("CARGO_PKG_NAME"),
+    short_message: LINT_MESSAGE,
+    long_message: "An overflow/underflow is typically caught and generates an error. When it is not caught, the operation will result in an inexact result which could lead to serious problems.",
+    severity: Severity::Critical,
+    help: "https://coinfabrik.github.io/scout-audit/docs/detectors/rust/overflow-check",
+    vulnerability_class: VulnerabilityClass::Arithmetic,
+};
+
+dylint_linting::declare_early_lint! {
+    pub OVERFLOW_CHECK,
+    Warn,
+    LINT_MESSAGE
+}
+
+impl OverflowCheck {
+    fn workspace_dir(&self) -> Result<PathBuf, String> {
+        // Locate the project workspace or package root
+        let output = Command::new(env!("CARGO"))
+            .arg("locate-project")
+            .arg("--workspace")
+            .arg("--message-format=plain")
+            .output()
+            .map_err(|e| format!("Failed to execute cargo command: {}", e))?;
+
+        if !output.status.success() {
+            return Err(format!(
+                "Cargo command failed with exit code: {}",
+                output.status
+            ));
+        }
+
+        // Convert the output to a string and parse it as a path
+        let path_str = from_utf8(&output.stdout)
+            .map_err(|_| "Output from cargo is not valid UTF-8".to_string())?
+            .trim();
+
+        // Find the parent directory, which should be the workspace directory
+        let cargo_path = Path::new(path_str);
+        cargo_path
+            .parent()
+            .ok_or_else(|| "Failed to find parent directory of the project".to_string())
+            .map(|path| path.to_path_buf())
+    }
+}
+
+impl EarlyLintPass for OverflowCheck {
+    fn check_crate(&mut self, cx: &EarlyContext<'_>, _: &Crate) {
+        // Attempt to get the workspace directory
+        let workspace_dir = match self.workspace_dir() {
+            Ok(dir) => dir,
+            Err(err) => {
+                cx.sess()
+                    .dcx()
+                    .struct_warn(format!("Failed to locate workspace directory: {}", err))
+                    .emit();
+                return;
+            }
+        };
+
+        // Attempt to read Cargo.toml
+        let cargo_toml_path = workspace_dir.join("Cargo.toml");
+        let contents = match fs::read_to_string(&cargo_toml_path) {
+            Ok(content) => content,
+            Err(e) => {
+                cx.sess()
+                    .dcx()
+                    .struct_warn(format!(
+                        "Failed to read Cargo.toml from {:?}: {}",
+                        cargo_toml_path, e
+                    ))
+                    .emit();
+                return;
+            }
+        };
+
+        // Attempt to parse Cargo.toml
+        let toml = match contents.parse::<Value>() {
+            Ok(parsed) => parsed,
+            Err(e) => {
+                cx.sess()
+                    .dcx()
+                    .struct_warn(format!("Failed to parse Cargo.toml: {}", e))
+                    .emit();
+                return;
+            }
+        };
+
+        // Check if the profile.release.overflow-checks is enabled
+        let overflow_checks = toml
+            .get("profile")
+            .and_then(|p| p.get("release"))
+            .and_then(|r| r.get("overflow-checks"));
+
+        // Check if overflow-checks is enabled
+        match overflow_checks {
+            Some(Value::Boolean(true)) => (), // All good
+            Some(_) | None => {
+                span_lint_and_help(
+                    cx,
+                    OVERFLOW_CHECK,
+                    DUMMY_SP,
+                    LINT_MESSAGE,
+                    None,
+                    "Enable overflow-checks on the release profile",
+                );
+            }
+        }
+    }
+}
