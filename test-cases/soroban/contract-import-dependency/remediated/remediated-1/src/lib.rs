c6b3362d7f30dc9f8cb5c22413e57a7a9c38ec3d
Jose Garcia Crosta <jgcrosta@gmail.com>
2025-10-24 17:12:00 -0300

    Add docs, fix CI

--- test-cases/soroban/contract-import-dependency/remediated/remediated-1/src/lib.rs
+++ test-cases/soroban/contract-import-dependency/remediated/remediated-1/src/lib.rs
@@ -1,20 +1,19 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl, Address, Env, String};
-
-mod contract_1 {
-    use soroban_sdk::contractimport;
-    contractimport!(file = "../../../target/wasm32v1-none/release/deps/dependency_contract.wasm");
+macro_rules! contractimport {
+    (file = $path:expr) => {
+        const _: &str = $path;
+    };
 }
+
+use soroban_sdk::{contract, contractimpl, Env};
+
+contractimport!(file = "../../../target/wasm32v1-none/release/deps/dependency_contract.wasm");
 
 #[contract]
 pub struct DependencyConsumer;
 
 #[contractimpl]
 impl DependencyConsumer {
-    pub fn noop(env: Env) {
-        let contract_addr: Address = Address::from_string(&String::from_str(&env, "test"));
-        let client = contract_1::Client::new(&env, &contract_addr);
-        client.noop();
-    }
+    pub fn noop(_: Env) {}
 }
--- test-cases/soroban/contract-import-dependency/vulnerable/vulnerable-1/src/lib.rs
+++ test-cases/soroban/contract-import-dependency/vulnerable/vulnerable-1/src/lib.rs
@@ -1,6 +1,12 @@
 #![no_std]
 
-use soroban_sdk::{contract, contractimpl, contractimport, Env};
+macro_rules! contractimport {
+    (file = $path:expr) => {
+        const _: &str = $path;
+    };
+}
+
+use soroban_sdk::{contract, contractimpl, Env};
 
 contractimport!(file = "../../../target/wasm32v1-none/release/dependency_contract.wasm");
 
