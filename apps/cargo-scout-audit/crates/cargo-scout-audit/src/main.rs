ad6cd89557ed753acc52141e0aaec18f567fa4b9
Jose Garcia Crosta <jgcrosta@gmail.com>
2025-11-05 12:41:26 -0300

    Add logging

--- apps/cargo-scout-audit/crates/cargo-scout-audit/src/main.rs
+++ apps/cargo-scout-audit/crates/cargo-scout-audit/src/main.rs
@@ -1,7 +1,22 @@
-use cargo_scout_audit::{cli_args, run::run_scout, util::print::print_full_error};
+use cargo_scout_audit::{
+    cli_args,
+    run::run_scout,
+    util::{
+        logger::{get_subscriber, init_subscriber},
+        print::print_full_error,
+    },
+};
 use clap::Parser;
+use tracing::level_filters::LevelFilter;
 
 fn main() {
+    let subscriber = get_subscriber(
+        "cargo-scout-audit".to_string(),
+        LevelFilter::INFO,
+        || std::io::stdout(),
+    );
+    init_subscriber(subscriber);
+
     let cli = cli_args::Cli::parse();
 
     match cli.subcmd {
