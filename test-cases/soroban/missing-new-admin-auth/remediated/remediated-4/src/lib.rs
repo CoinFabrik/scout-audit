62da7fdbceca31b73ff1e79343715d247d3c09df
Víctor M. González <helios.vmg@gmail.com>
2026-01-20 21:42:11 -0300

    Added test cases.

---
+++ test-cases/soroban/missing-new-admin-auth/remediated/remediated-4/src/lib.rs
@@ -0,0 +1,31 @@
+#![no_std]
+
+use soroban_sdk::{contract, contractimpl, contracttype, Address, Env};
+
+#[contracttype]
+pub enum DataKey {
+    Admin,
+}
+
+#[contract]
+pub struct MissingNewAdminAuth;
+
+#[contractimpl]
+impl MissingNewAdminAuth {
+    pub fn initialize(e: Env, admin: Address) {
+        e.storage().instance().set(&DataKey::Admin, &admin);
+    }
+
+    fn foo(x: &Address){
+        x.require_auth();
+    }
+
+    pub fn set_admin(e: Env, new_admin: Address, b: bool) {
+        let admin: Address = e.storage().instance().get(&DataKey::Admin).unwrap();
+        admin.require_auth();
+        if true{
+            new_admin.require_auth();
+        }
+        e.storage().instance().set(&DataKey::Admin, &new_admin);
+    }
+}
