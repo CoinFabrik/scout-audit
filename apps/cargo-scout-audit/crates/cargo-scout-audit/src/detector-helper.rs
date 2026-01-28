e1489a19ec583d741f27035b97a52edaa28d4b6a
Jose Garcia Crosta <jgcrosta@gmail.com>
2026-01-28 12:44:23 -0300

    Move `detector-helper` from `cargo-scout-audit` to `scout-driver`

--- apps/cargo-scout-audit/crates/cargo-scout-audit/src/detector-helper.rs
+++ apps/cargo-scout-audit/crates/cargo-scout-audit/src/detector-helper.rs
@@ -3,7 +3,7 @@ use crate::{
     interop::helper::{HelperInput, HelperOutput},
     util::{build_and_run::PackageToBuild, detectors_info::LintStore},
 };
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
 use std::path::PathBuf;
 
 pub fn get_detectors_info(
@@ -17,12 +17,12 @@ pub fn get_detectors_info(
 
     let mut pkg = match scout_sources {
         Some(root) => PackageToBuild::new_local(root.clone()),
-        None => PackageToBuild::new_remote(SCOUT_REPO, SCOUT_BRANCH, "cargo-scout-audit"),
+        None => PackageToBuild::new_remote(SCOUT_REPO, SCOUT_BRANCH, "scout-driver"),
     };
     pkg.build_message = "Building detector-helper".to_string();
     pkg.build_error_message = "Failed to build detector-helper".to_string();
-    pkg.internal_path = Some("apps/cargo-scout-audit/crates/cargo-scout-audit".into());
-    let path = pkg.build_executable(Some("cargo-scout-audit"), "detector-helper")?;
+    pkg.internal_path = Some("apps/cargo-scout-audit/crates/scout-driver".into());
+    let path = pkg.build_executable(Some("scout-driver"), "detector-helper")?;
 
     let output =
         crate::interop::subprocess::run_subprocess::<_, HelperOutput>(toolchain, &path, &input)?;
